409e430f6f3b72480d2fdf0ee834a61fbc3809f3
Oleg24280 <opbobrov@gmail.com>
2024-09-14 23:37:12 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,7 @@ name = "Oleg"
 print("Name:", name)
 age = 66
 print("Age:", age)
-new_age = age + 1
-age = new_age
+age = age + 1
 print("New Age:", age)
 is_student = True
 print("Is Studeht:", is_student)
